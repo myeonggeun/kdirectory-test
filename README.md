c6663a9d9e7708132e98a70de24647c5af23c1ae
myeokim <musashi501@gmail.com>
2025-10-14 21:47:02 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# kdirectory-test
---
+++ kdata.py
@@ -0,0 +1,8 @@
+
+
+Korea_directory = [
+{"Name":"김명근","Mobile":"010-9323-6574","Email":"myeokim"},
+{"Name":"박병철","Mobile":"010-8845-7226","Email":"byupark"},
+{"Name":"김문석","Mobile":"010-5919-7276","Email":"mokim"},
+{"Name":"임재경","Mobile":"010-5172-8340","Email":"jaelim"},
+]
