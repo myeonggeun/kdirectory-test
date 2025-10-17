b7ba249bbc6bc8a71b69a7a905bfc22212350b28
myeokim <musashi501@gmail.com>
2025-10-17 13:52:00 +0900

    kdata revised

--- convertCsvToDict.py
+++ convertCsvToDict.py
@@ -1,7 +1,10 @@
 # import csv
 import csv
+from kdata import *
 # read csv file to a list of dictionaries
-with open('data.csv', 'r') as file:
+with open('data.csv', 'r', encoding='utf-8-sig' ) as file:
     csv_reader = csv.DictReader(file)
     data = [row for row in csv_reader]
-print(data)
+    
+Korea_directory = data
+print(Korea_directory)
